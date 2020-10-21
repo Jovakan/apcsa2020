cbc9d769ef5db26d4fbad8e681708144d83d6490
matt <matthew.f.prado@valkyriepcs.com>
2020-10-21 08:13:55 -0700

    Corrected various bugs.

--- unit4/lesson2/U4L2Q3.java
+++ unit4/lesson2/U4L2Q3.java
@@ -1,6 +1,3 @@
-/* OMIT THIS WHEN DONE */
-/* PLEASE READ: EDHESIVE HAS MADE MISTAKES WITH THEIR AUTOGRADER. THE OUTPUT MATCHES, BUT YOU'LL NEED TO CONTACT YOUR TEACHER TO APPROVE IT. */
-
 /* Lesson 2 Coding Activity Question 3 */
 
 import java.util.Scanner;
@@ -20,6 +17,7 @@ public class U4_L2_Activity_Three{
     double maxlong = -180;
     double maxlat = -90;
     
+    /* While loop for any additional points */
     while(cont == 1) {
     incorr = 0;
     System.out.println("Please enter the longitude:");
@@ -27,33 +25,34 @@ public class U4_L2_Activity_Three{
       if (currlong < -180 || currlong > 180) {
       System.out.println("Incorrect Latitude or Longitude");
       incorr = 1;
-      }  
+      }
+      
     System.out.println("Please enter the latitude:");
     currlat = scan.nextDouble();
-      if (currlat < -180 || currlat > 180) {
+      if (currlat < -90 || currlat > 90) {
       System.out.println("Incorrect Latitude or Longitude");
       incorr = 1;    
       }
-      
-    if(incorr == 0) {
+      if(incorr == 0) {
       if(currlong < minlong)
       minlong = currlong;
       
       if(currlong > maxlong)
       maxlong = currlong;
       
+      
       if(currlat < minlat)
       minlat = currlat;
       
       if(currlat > maxlat)
-      maxlat = currlat; 
-      }
+      maxlat = currlat; }
       
     System.out.println("Would you like to enter another location (1 for yes, 0 for no)?");
     cont = scan.nextInt();
     
     }
-    System.out.println("Fartest North: " + maxlat);
+    
+    System.out.println("Farthest North: " + maxlat);
     System.out.println("Farthest South: " + minlat);
     System.out.println("Farthest East: " + maxlong);
     System.out.println("Farthest West: " + minlong);
