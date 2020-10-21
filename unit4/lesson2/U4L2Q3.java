95b6077f3d7d09ebfba759b64a1105e62bd9c709
matt <matthew.f.prado@valkyriepcs.com>
2020-10-20 17:26:02 -0700

    Fixed formatting and spacing

--- unit4/lesson2/U4L2Q3.java
+++ unit4/lesson2/U4L2Q3.java
@@ -20,7 +20,6 @@ public class U4_L2_Activity_Three{
     double maxlong = -180;
     double maxlat = -90;
     
-    /* While loop for any additional points */
     while(cont == 1) {
     incorr = 0;
     System.out.println("Please enter the longitude:");
@@ -28,33 +27,32 @@ public class U4_L2_Activity_Three{
       if (currlong < -180 || currlong > 180) {
       System.out.println("Incorrect Latitude or Longitude");
       incorr = 1;
-      }
-      
+      }  
     System.out.println("Please enter the latitude:");
     currlat = scan.nextDouble();
       if (currlat < -180 || currlat > 180) {
       System.out.println("Incorrect Latitude or Longitude");
       incorr = 1;    
       }
-      if(incorr == 0) {
+      
+    if(incorr == 0) {
       if(currlong < minlong)
       minlong = currlong;
       
       if(currlong > maxlong)
       maxlong = currlong;
       
-      
       if(currlat < minlat)
       minlat = currlat;
       
       if(currlat > maxlat)
-      maxlat = currlat; }
+      maxlat = currlat; 
+      }
       
     System.out.println("Would you like to enter another location (1 for yes, 0 for no)?");
     cont = scan.nextInt();
     
     }
-    
     System.out.println("Fartest North: " + maxlat);
     System.out.println("Farthest South: " + minlat);
     System.out.println("Farthest East: " + maxlong);
