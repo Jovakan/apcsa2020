10b8227c0570cf307ca6217511a41e00538cf65e
matt <matthew.f.prado@valkyriepcs.com>
2020-10-21 22:12:30 -0700

    Create U4L3Q1.java

---
+++ unit4/lesson3/U4L3Q1.java
@@ -0,0 +1,14 @@
+/* Lesson 3 Coding Activity Question 1 */
+
+import java.util.Scanner;
+
+public class U4_L3_Activity_One{
+  public static void main(String[] args){
+    
+    /* Write your code here */
+    for(int i = 23; i <= 89; i++){
+    System.out.print(i + " ");
+    if(i % 10 == 2)
+    System.out.print("\n"); }
+  }
+}
