736735b6c062f6effc078382a22c6d06a3472d6a
matt <matthew.f.prado@valkyriepcs.com>
2020-10-25 14:42:58 -0700

    Create U4L5Q2.java

---
+++ unit4/lesson5/U4L5Q2.java
@@ -0,0 +1,19 @@
+/* Lesson 5 Coding Activity Question 2 */
+
+import java.util.Scanner;
+
+public class U4_L5_Activity_Two{
+  public static void main(String[] args){
+    
+    /* Write your code here */
+    for (int i = 1; i <= 20; i++) 
+{
+  for (int j = 0; j < i; j++) 
+  {
+    System.out.print(i + " ");
+  }
+  System.out.println();
+}
+
+  }
+}
