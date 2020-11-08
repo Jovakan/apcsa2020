2ca3f046091b1e9140672aa14530b019cb9ddd9a
matt <matthew.f.prado@valkyriepcs.com>
2020-11-08 14:27:20 -0800

    Create U5L3Q3.java

---
+++ unit5/lesson3/U5L3Q3.java
@@ -0,0 +1,20 @@
+/* Lesson 3 Coding Activity Question 3 */
+
+import java.util.Scanner;
+import edhesive.shapes.*;
+
+public class U5_L3_Activity_Three{
+
+  /* Add the method updateRadius here */
+  public static void updateRadius(Circle a, int b) {
+    a.setRadius(b);
+  }
+
+  // You can uncomment and add to the main method to test your code
+  // You will need to remove/comment out this method before checking your code for a score
+  /*
+  public static void main(String[] args){
+    
+  }
+  */
+}
