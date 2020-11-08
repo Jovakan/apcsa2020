a2ed62ad5bbb09a6b27c0154a702dc0ce8acfad9
matt <matthew.f.prado@valkyriepcs.com>
2020-11-08 13:50:38 -0800

    Create U5L2Q3.java

---
+++ unit5/lesson2/U5L2Q3.java
@@ -0,0 +1,20 @@
+/* Lesson 2 Coding Activity Question 3 */
+
+import java.util.Scanner;
+
+public class U5_L2_Activity_Three{
+
+  /* Add the method swap here */
+  public static void swap(int a, double b) {
+    System.out.print(b + " ");
+    System.out.print(a);
+  }
+
+  // You can uncomment and add to the main method to test your code
+  // You will need to remove/comment out this method before checking your code for a score
+  /*
+  public static void main(String[] args){
+    
+  }
+  */
+}
