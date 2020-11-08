187ed2ac704c481fa3f2d2bb5853c41ed31af8f9
matt <matthew.f.prado@valkyriepcs.com>
2020-11-08 13:15:54 -0800

    Create U5L1Q1.java

---
+++ unit5/lesson1/U5L1Q1.java
@@ -0,0 +1,17 @@
+/* Lesson 1 Coding Activity Question 1 */
+
+import java.util.Scanner;
+
+public class U5_L1_Activity_One{
+
+  /* Add the method myMethod here */
+
+  // You can uncomment and add to the main method to test your code
+  // You will need to remove/comment out this method before checking your code for a score
+  
+  //public static void main(String[] args){
+  
+  public static void myMethod() {
+    System.out.println("This is a void method");
+  }
+}
