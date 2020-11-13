1d52b49939033b08c90c92fd418bd2b4a5276410
matt <matthew.f.prado@valkyriepcs.com>
2020-11-12 19:35:44 -0800

    Create U5L4Q2.java

---
+++ unit5/lesson4/U5L4Q2.java
@@ -0,0 +1,20 @@
+/* Lesson 4 Coding Activity Question 2 */
+
+import java.util.Scanner;
+
+public class U5_L4_Activity_Two{
+
+public static double average(int a, int b, int c, int d, int e) {
+  double z = (double) (a + b + c + d + e) / 5;
+  return z;
+}
+  /* Add the method average here */
+
+  // You can uncomment and add to the main method to test your code
+  // You will need to remove/comment out this method before checking your code for a score
+  /*
+  public static void main(String[] args){
+    
+  }
+  */
+}
