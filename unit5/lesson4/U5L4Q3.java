e2b921ff4168eb673854007bde6e18e006352422
matt <matthew.f.prado@valkyriepcs.com>
2020-11-12 19:35:28 -0800

    Create U5L4Q3.java

---
+++ unit5/lesson4/U5L4Q3.java
@@ -0,0 +1,23 @@
+/* Lesson 4 Coding Activity Question 3 */
+
+import java.util.Scanner;
+import edhesive.shapes.*;
+
+public class U5_L4_Activity_Three{
+  
+  public static double areaDiff(Rectangle a, Rectangle b) {
+    Double c = Math.abs(a.getArea() - b.getArea());
+    return c;
+    
+  }
+
+  /* Add the method areaDiff here */
+
+  // You can uncomment and add to the main method to test your code
+  // You will need to remove/comment out this method before checking your code for a score
+  /*
+  public static void main(String[] args){
+    
+  }
+  */
+}
