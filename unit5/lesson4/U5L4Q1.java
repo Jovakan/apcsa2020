106b93b05b46dc332a5b2f8966a8244080243ff5
matt <matthew.f.prado@valkyriepcs.com>
2020-11-12 19:35:56 -0800

    Create U5L4Q1.java

---
+++ unit5/lesson4/U5L4Q1.java
@@ -0,0 +1,22 @@
+/* Lesson 4 Coding Activity Question 1 */
+
+import java.util.Scanner;
+import edhesive.shapes.*;
+
+public class U5_L4_Activity_One{
+
+  /* Add the method areaSum here */
+  
+  public static double areaSum(Circle a, Circle b) {
+    double c = a.getArea() + b.getArea();
+    return c;
+  }
+
+  // You can uncomment and add to the main method to test your code
+  // You will need to remove/comment out this method before checking your code for a score
+  /*
+  public static void main(String[] args){
+    
+  }
+  */
+}
